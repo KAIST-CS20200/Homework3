48204cc4c525989fa074499391102c2b143983bc
Sang Kil Cha <sangkilc@kaist.ac.kr>
2024-02-12 15:30:54 +0900

    Add HW3

---
+++ Program.fs
@@ -0,0 +1,78 @@
+﻿module CS220.Program
+
+type AMPM =
+  | AM
+  | PM
+
+/// Time is a record type that represents a time of day. It has three fields:
+/// Hours, Minutes, and AMPM. Hours and Minutes are integers, and AMPM is a
+/// discriminated union with two cases: AM and PM. For example, 3:45 PM is
+/// represented as { Hours = 3; Minutes = 45; AMPM = PM }.
+type Time = {
+  Hours: int
+  Minutes: int
+  AMPM: AMPM
+}
+
+/// Write this function `diffMinutes` that  takes in two Time values t1 and t2
+/// and returns how many minutes t1 is earlier than t2. For example, when t1 is
+/// { Hours = 1; Minutes = 30; AMPM = AM } and t2 is { Hours = 1; Minutes = 0;
+/// AMPM = PM }, then the function should return 690. If t1 is later than t2,
+/// the function should return 0. You can assume that t1 and t2 are on the same
+/// day. You can also assume that both t1 and t2 are well-formed (i.e., hours
+/// are between 1 and 12, and minutes are between 0 and 59).
+let diffMinutes t1 t2 =
+  failwith "TODO"
+
+/// StraightLine represents a line in a 2D plane where the line is represented
+/// by the equation y = ax + b. The first component is the slope a, and the
+/// second component is the y-intercept b. For example, the line y = 3x + 2 is
+/// represented as (3, 2).
+type StraightLine = int * int
+
+/// Write a function `mirrorX` that takes in a StraightLine and returns a new
+/// StraightLine that represents the mirror image of the given line with respect
+/// to the x-axis.
+let mirrorX (line: StraightLine) =
+  failwith "TODO"
+
+/// Write a function `mirrorY` that takes in a StraightLine and returns a new
+/// StraightLine that represents the mirror image of the given line with respect
+/// to the y-axis.
+let mirrorY (line: StraightLine) =
+  failwith "TODO"
+
+/// Write a function `removeOdd` that takes an integer list (int list) as input
+/// and returns a new integer list without odd numbers. The resulting list
+/// should preserve the order of the original list. For example, given [ 1; 2; 3
+/// ] the function should return [ 2 ].
+let removeOdd (lst: int list) =
+  failwith "TODO"
+
+/// Write a function `getSmallest` that takes in list of integers as input and
+/// returns the smallest number in the list (if exists). For example, given [ 1;
+/// 2; 3 ] the function will return Some 1. If the given list is empty, than the
+/// function simply returns None.
+let getSmallest lst =
+  failwith "TODO"
+
+/// Write a function `take` that takes in a list and an unsigned integer n
+/// (uint32) as input, and returns the first n elements from the list. For
+/// example, take [ "a"; "b"; "c" ] 1u returns [ "a" ]. If n is larger than the
+/// length of the list, then the function returns the list as it is. When n is
+/// zero, the function returns an empty list.
+let take lst (n: uint32) =
+  failwith "TODO"
+
+/// Write a function `runLength` that takes in a list and returns a list of
+/// pairs by applying run-length encoding. Specifically, the function replace n
+/// consecutive elements into a pair of (element, number of appearance). For
+/// example, when a list [ 1; 2; 2; 2; 3; 3 ] is given, the function returns [
+/// (1, 1); (2, 3); (3, 2) ], because 1 consecutively appears once, 2 appears
+/// three times, and 3 appears twice.
+let runLength lst =
+  failwith "TODO"
+
+[<EntryPoint>]
+let main _args =
+  0
---
+++ Tests/Tests.fs
@@ -0,0 +1,135 @@
+﻿namespace CS220
+
+open CS220.Program
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.A``() =
+    let t1 = { Hours = 1; Minutes = 30; AMPM = AM }
+    let t2 = { Hours = 1; Minutes = 0; AMPM = PM }
+    let r = diffMinutes t1 t2
+    Assert.AreEqual (690, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.B``() =
+    let t1 = { Hours = 1; Minutes = 30; AMPM = AM }
+    let t2 = { Hours = 1; Minutes = 0; AMPM = AM }
+    let r = diffMinutes t1 t2
+    Assert.AreEqual (0, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.C``() =
+    let t1 = { Hours = 10; Minutes = 0; AMPM = PM }
+    let t2 = { Hours = 10; Minutes = 0; AMPM = PM }
+    let r = diffMinutes t1 t2
+    Assert.AreEqual (0, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "1">]
+  member __.``Problem 1.D``() =
+    let t1 = { Hours = 10; Minutes = 5; AMPM = PM }
+    let t2 = { Hours = 1; Minutes = 0; AMPM = AM }
+    let r = diffMinutes t1 t2
+    Assert.AreEqual (0, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "2">]
+  member __.``Problem 2.A``() =
+    let r = StraightLine (4, 4) |> mirrorX
+    Assert.AreEqual (StraightLine (-4, -4), r)
+
+  [<TestMethod; Timeout 1000; TestCategory "2">]
+  member __.``Problem 2.B``() =
+    let r = StraightLine (-1, 1) |> mirrorY
+    Assert.AreEqual (StraightLine (1, 1), r)
+
+  [<TestMethod; Timeout 1000; TestCategory "2">]
+  member __.``Problem 2.C``() =
+    let r = StraightLine (-1, -1) |> mirrorX
+    Assert.AreEqual (StraightLine (1, 1), r)
+
+  [<TestMethod; Timeout 1000; TestCategory "2">]
+  member __.``Problem 2.D``() =
+    let r = StraightLine (20, 20) |> mirrorY
+    Assert.AreEqual (StraightLine (-20, 20), r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.A``() =
+    let r = removeOdd [ 1; 2; 3; 4 ]
+    Assert.AreEqual ([ 2; 4 ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.B``() =
+    let r = removeOdd [ 3; 5; 1 ]
+    Assert.AreEqual ([], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.C``() =
+    let r = removeOdd [ 0 ]
+    Assert.AreEqual ([ 0 ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "3">]
+  member __.``Problem 3.D``() =
+    let r = removeOdd [ -3; -2; -1; 0; 1; 2; 3 ]
+    Assert.AreEqual ([ -2; 0; 2 ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.A``() =
+    let r = getSmallest [ 1; 2; 3; 4 ]
+    Assert.AreEqual (Some 1, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.B``() =
+    let r = getSmallest [ 1; 0; -1 ]
+    Assert.AreEqual (Some -1, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.C``() =
+    let r = getSmallest [ 0 ]
+    Assert.AreEqual (Some 0, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "4">]
+  member __.``Problem 4.D``() =
+    let r = getSmallest []
+    Assert.AreEqual (None, r)
+
+  [<TestMethod; Timeout 1000; TestCategory "5">]
+  member __.``Problem 5.A``() =
+    let r = take [ 1; 2; 3 ] 1u
+    Assert.AreEqual ([ 1 ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "5">]
+  member __.``Problem 5.B``() =
+    let r = take [ "a"; "b"; "c" ] 2u
+    Assert.AreEqual ([ "a"; "b" ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "5">]
+  member __.``Problem 5.C``() =
+    let r = take [ 1.1; 2.2; 3.3; 4.4 ] 5u
+    Assert.AreEqual ([ 1.1; 2.2; 3.3; 4.4 ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "5">]
+  member __.``Problem 5.D``() =
+    let r = take [ "a"; "b"; "c" ] 0u
+    Assert.AreEqual ([], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "6">]
+  member __.``Problem 6.A``() =
+    let r = runLength [ "1"; "2"; "2"; "2"; "3"; "3" ]
+    Assert.AreEqual ([ ("1", 1); ("2", 3); ("3", 2) ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "6">]
+  member __.``Problem 6.B``() =
+    let r = runLength []
+    Assert.AreEqual ([], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "6">]
+  member __.``Problem 6.C``() =
+    let r = runLength [ 1; 1; 2; 3; 3; 4; 4; 4; 3; 2 ]
+    Assert.AreEqual ([ (1, 2); (2, 1); (3, 2); (4, 3); (3, 1); (2, 1) ], r)
+
+  [<TestMethod; Timeout 1000; TestCategory "6">]
+  member __.``Problem 6.D``() =
+    let r = runLength [ 100; 100; 100; 100; 100 ]
+    Assert.AreEqual ([ (100, 5) ], r)
